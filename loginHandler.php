5a1cac29606ba38e1b06add643620048b74f4b29
Kdkocian <krencent870@Gmail.com>
2019-04-28 21:07:07 -0500

    changed more things

--- loginHandler.php
+++ loginHandler.php
@@ -1,26 +1,38 @@
 
 <?php
-require ('myfuncs.php');
-$conn =  dbConnect();
-
-$Uname = mysqli_real_escape_string($conn, $_POST['Uname']);
-$Pword = mysqli_real_escape_string($conn, $_POST['Pword']);
-if ($Uname == (NULL)){
-    echo " The First Name cannot be empty";
-}
-if ($Pword == (NULL)){
-    echo " The Password field is empty";
-}
-
-// checks to match username and password and logs in the users
-$sql = "SELECT * From users Where USERNAME = '$Uname' and PASSWORD = '$Pword';";
+    //Open a connection
+    require('myfuncs.php');
+    $conn = dbConnect();
+    if(mysqli_error($conn))
+    die("Connection Failed: " . mysqli_connect_error());
+    
+    $username = $_POST["Uname"];
+    if($username == null)
+    {
+        echo 'You must fill out username';
+        exit;
+    }
+    $password = $_POST["Pword"];
+    if($password == null)
+    {
+        echo 'You must fill out the password';
+        exit;
+    }
+    $sql = "SELECT * FROM user WHERE username='$username'";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-    if ($row['USERNAME'] == $Uname && $row['PASSWORD'] == $Pword ){
-        saveUerId($row["ID"]);
-        include 'loginResponse.php';
-    } else{
-        $message = 'Login Failed';
-        include 'loginFailed.php';
+    if($row["username"] == $username && $row["password"] == $password)
+    {
+        saveUserId($row["id"]);
+        include('loginResponse.php');
     }
+        
+    else 
+    {
+        $message = "Login Failed";
+        include('loginFailed.php');
+    }
+
+    mysqli_close($conn)
 ?>
+<a href="index.html">Main Menu</a>
