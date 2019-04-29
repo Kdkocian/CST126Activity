6e4add2d95c5447bb2a9997d676dbd18c8a1f89b
Kdkocian <krencent870@Gmail.com>
2019-04-28 20:37:35 -0500

    changed connection

--- myfuncs.php
+++ myfuncs.php
@@ -5,7 +5,7 @@ function dbConnect() {
     $password = '7d31dba5';
     $dbname = 'heroku_ff4f00db4526749';
     //creates connection
-   $conn =  new mysqli($servername, $username, $password, $dbname);
+   $conn =  mysqli_connect($servername, $username, $password, $dbname);
     //tests the connection
     if ($conn->connect_error){
         die("Connection failed: " . $conn->connect_error);
