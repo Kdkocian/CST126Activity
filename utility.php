da5091e973fb843569bfc31461740ae302cda8d5
Kdkocian <krencent870@Gmail.com>
2019-04-29 00:02:31 -0500

    display user will work

--- utility.php
+++ utility.php
@@ -5,7 +5,7 @@ function getAllusers(){
     $conn = $db -> dbConnect();
        $sql = "SELECT ID, First_Name, Last_Name FROM users";
          $users = array();  
-       $result = $conn->query($sql);
+       $result = mysqli_query($conn, $sql);
        
            while ($row = mysqli_fetch_array($result)){
                $users[] = array($row['ID'], $row['First_Name'], $row['Last_Name']);
