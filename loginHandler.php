161b2a2b53c8d6e2dfc1edd06132e1eb9653086f
Kdkocian <krencent870@Gmail.com>
2019-04-28 20:29:37 -0500

    changed array to assoc

--- loginHandler.php
+++ loginHandler.php
@@ -19,7 +19,7 @@ if ($Pword == ($blank or NULL)){
 // checks to match username and password and logs in the users
     $result = mysqli_query($conn, "SELECT * From users Where USERNAME = '$Uname' and PASSWORD = '$Pword';")
         or die ("Failed to query database". mysqli_connect_error());
-    $row = mysqli_fetch_array($result);
+    $row = mysqli_fetch_assoc($result);
     $_SESSION['userID']  = $row['ID'];// Save User ID in the Session
     if ($row['USERNAME'] == $Uname && $row['PASSWORD'] == $Pword ){
         saveUerId($row["ID"]);
