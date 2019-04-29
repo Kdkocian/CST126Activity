63a6a51a9fbb1cfe316a10db061d974342e086fa
Kdkocian <krencent870@Gmail.com>
2019-04-28 19:54:22 -0500

    assigned new database on heroku

--- myfuncs.php
+++ myfuncs.php
@@ -1,10 +1,10 @@
 <?php
 class myfuncs{
 function dbConnect() {
-    $servername = 'localhost';
-    $username = 'bubblepop';
-    $password = 'bubbles';
-    $dbname = 'users';
+    $servername = 'us-cdbr-iron-east-02.cleardb.net';
+    $username = 'bf8f2518ac88f5';
+    $password = '7d31dba5';
+    $dbname = 'heroku_ff4f00db4526749';
     //creates connection
    $conn =  new mysqli($servername, $username, $password, $dbname);
     //tests the connection
