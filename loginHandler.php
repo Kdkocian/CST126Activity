20292d06a62c6f6cd30f319dee0f39c22b3931de
Kdkocian <krencent870@Gmail.com>
2019-04-28 19:59:21 -0500

    changed loginHandler for streamline

--- loginHandler.php
+++ loginHandler.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 $blank = "";
-$error = "";
 $Uname = "";
 $Pword = "";
 include 'myfuncs.php';
@@ -10,16 +9,16 @@ $conn = $db -> dbConnect();
 
 $Uname = mysqli_real_escape_string($conn, $_POST['Uname']);
 $Pword = mysqli_real_escape_string($conn, $_POST['Pword']);
-if ("$Uname" == ($blank or NULL)){
+if ($Uname == ($blank or NULL)){
     echo " The First Name cannot be empty";
 }
-if ("$Pword" == ($blank or NULL)){
+if ($Pword == ($blank or NULL)){
     echo " The Password field is empty";
 }
 
 // checks to match username and password and logs in the users
     $result = mysqli_query($conn, "SELECT * From users Where USERNAME = '$Uname' and PASSWORD = '$Pword'")
-        or die ("Failed to query database". mysql_error());
+        or die ("Failed to query database". mysqli_connect_error());
     $row = mysqli_fetch_array($result);
     $_SESSION['userID']  = $row['ID'];// Save User ID in the Session
     if ($row['USERNAME'] == $Uname && $row['PASSWORD'] == $Pword ){
