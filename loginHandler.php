3424968c157462fab624da68cae37e26059bc419
Kdkocian <krencent870@Gmail.com>
2019-04-28 20:48:34 -0500

    changed connection

--- loginHandler.php
+++ loginHandler.php
@@ -1,25 +1,22 @@
 
 <?php
 session_start();
-$blank = "";
-$Uname = "";
-$Pword = "";
 require ('myfuncs.php');
 $conn =  dbConnect();
 
 $Uname = mysqli_real_escape_string($conn, $_POST['Uname']);
 $Pword = mysqli_real_escape_string($conn, $_POST['Pword']);
-if ($Uname == ($blank or NULL)){
+if ($Uname == (NULL)){
     echo " The First Name cannot be empty";
 }
-if ($Pword == ($blank or NULL)){
+if ($Pword == (NULL)){
     echo " The Password field is empty";
 }
 
 // checks to match username and password and logs in the users
-    $result = mysqli_query($conn, "SELECT * From users Where USERNAME = '$Uname' and PASSWORD = '$Pword';")
-        or die ("Failed to query database". mysqli_connect_error());
-    $row = mysqli_fetch_assoc($result);
+$sql = "SELECT * From users Where USERNAME = '$Uname' and PASSWORD = '$Pword';";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
     $_SESSION['userID']  = $row['ID'];// Save User ID in the Session
     if ($row['USERNAME'] == $Uname && $row['PASSWORD'] == $Pword ){
         saveUerId($row["ID"]);
@@ -28,4 +25,4 @@ if ($Pword == ($blank or NULL)){
         $message = 'Login Failed';
         include 'loginFailed.php';
     }
-    ?>
+?>
