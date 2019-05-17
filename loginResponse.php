edd56186d478ff24e1cc716cbf5cd89d7bd7bbbf
Kdkocian <krencent870@Gmail.com>
2019-05-17 17:51:15 -0500

    trying new insert users logic to display the new users

--- loginResponse.php
+++ loginResponse.php
@@ -1,15 +1,16 @@
-
+<?php 
+include 'utility.php';
+$utility = new utility;
+$utility->insertUsers();
+$user = new utility();
+$user->getAllusers();
+?>
 <html>
 <head>
 </head>
 <body>
 <h2>Login was successful: <?php echo "" . $Uname ?></h2>
 <?php include ('_displayUser.php');?>
-<?php 
-include 'utility.php';
-$utility = new utility;
-$utility->insertUsers();
-?>
 <a href="whoAmI.php">Who Am I</a>
 </body>
-</html>7
+</html>
